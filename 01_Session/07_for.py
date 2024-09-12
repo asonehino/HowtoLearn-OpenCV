1bb9961847a6357a8e7d520acb8e90057850dabd
hino <asonehino@naver.com>
2024-09-12 13:48:37 +0900

    for

---
+++ 01_Session/07_for.py
@@ -0,0 +1,21 @@
+#반복문
+collection = ["apple", "melon", "banana", "orange"]
+for fruit in collection:
+  print(fruit)
+print("-------------------")
+
+num = 0
+for num in range(5):
+  print(num)
+print("-------------------")
+
+num = 0
+for num in range(0,9,2):
+  print(num)
+print("-------------------")
+
+arr = []
+for i in range():
+  print(arr[i])
+print(arr)
+print("-------------------")
